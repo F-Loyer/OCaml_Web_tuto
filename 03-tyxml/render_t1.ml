4f05fa490a7e62395ae261d06898a91f299be8ca
Loyer <loyer@audiokeys.net>
2026-05-02 18:35:21 +0200

    03-tyxml: fix the layout with CSS table display attribute

--- 03-tyxml/render_t1.ml
+++ 03-tyxml/render_t1.ml
@@ -9,18 +9,19 @@ let render ~table ~logged ~csrf_token =
         [
           style [
             txt {css|
-h1 {
-  border-bottom-style:solid;
-  border-bottom-width:5px;
-  border-bottom-color:#844;
-  font-family: Verdana, sans-serif; 
-  font-style: italic;
-}
+                 h1 {
+                 border-bottom-style:solid;
+                 border-bottom-width:5px;
+                 border-bottom-color:#08C;
+                 font-family: Verdana, sans-serif; 
+                 font-style: italic;
+                 }
 
-    table { border-collapse: collapse; border: 2px solid #08c;}
-    td, th { border: 1px solid #08c; padding: 8px; }
-    input { width:300px; margin:5px; }
-    button { margin:5px; }
+                 .table { display:table; border-collapse: collapse; border: 2px solid #08c;}
+                 .row { display:table-row; }
+                 .cell { display:table-cell; 1px solid #08c; padding: 8px;}
+                 input { width:300px; margin:5px; }
+                 button { margin:5px; }
                  |css} ]
      ])
      (body (
@@ -44,53 +45,61 @@ h1 {
         @
         [
           h1 [ txt "T1 table" ];
-          table (
-            [
-              tr ~a:[ a_style "background: #08C" ] [
-                td [
-                  form ~a:[ a_action "#"; a_method `Post ] [
-                    input ~a:[ a_input_type `Hidden;
-                               a_name "dream.csrf";
-                               a_value csrf_token ] ();
-                    input ~a:[ a_input_type `Text;
-                               a_name "value"; a_value "" ] ();
+          div ~a:[ a_class [ "table" ]] ([
+              form ~a:[ a_class [ "row" ];
+                        a_action "#"; a_method `Post ] [
+                  div ~a:[ a_class [ "cell" ]] [
+                      txt "Value"
+                    ];
+                  div ~a:[ a_class [ "cell" ]] [
+                      txt "Actions"
+                    ]
+                ];
+              form ~a:[ a_class [ "row" ]; a_style "background: #08C";
+                        a_action "#"; a_method `Post ] [
+                  div ~a:[ a_class [ "cell" ]] [
+                      input ~a:[ a_input_type `Hidden;
+                                 a_name "dream.csrf";
+                                 a_value csrf_token ] ();
+                      input ~a:[ a_input_type `Text;
+                                 a_name "value"; a_value "" ] ();
+                    ];
+                  div ~a:[ a_class [ "cell" ]] [
                     button ~a:[ a_button_type `Submit;
                                 a_name "action";
                                 a_text_value "insert" ]
                       [ txt "Insert" ]
                     ]
-                  ]
-                ] 
+                ]
             ]
             @
             List.map (fun (id, value) ->
-              tr [
-                td [
-                  form ~a:[ a_action "#"; a_method `Post ] [
-                      input ~a:[ a_input_type `Hidden;
-                                 a_name "id";
-                                 a_value (string_of_int id) ] ();
-                      input ~a:[ a_input_type `Hidden;
-                                 a_name "dream.csrf";
-                                 a_value csrf_token ] ();
-                      input ~a:[ a_input_type `Text;
-                                 a_name "value";
-                                 a_value value ] ();
-                      button ~a:[ a_button_type `Submit;
-                                  a_name "action";
-                                  a_text_value "change" ]
-                        [ txt "Change" ];
-                      button ~a:[ a_button_type `Submit;
-                                  a_name "action";
-                                  a_text_value "delete" ]
-                        [ txt "Delete" ];
-                    ]
+                form ~a:[ a_class [ "row" ];
+                          a_action "#"; a_method `Post ] [
+                    div ~a:[ a_class [ "cell" ]] [
+                        input ~a:[ a_input_type `Hidden;
+                                   a_name "id";
+                                   a_value (string_of_int id) ] ();
+                        input ~a:[ a_input_type `Hidden;
+                                   a_name "dream.csrf";
+                                   a_value csrf_token ] ();
+                        input ~a:[ a_input_type `Text;
+                                   a_name "value";
+                                   a_value value ] ();
+                      ];
+                      div ~a:[ a_class [ "cell" ]] [
+                        button ~a:[ a_button_type `Submit;
+                                    a_name "action";
+                                    a_text_value "change" ]
+                          [ txt "Change" ];
+                        button ~a:[ a_button_type `Submit;
+                                    a_name "action";
+                                    a_text_value "delete" ]
+                          [ txt "Delete" ];
+                      ]
                   ]
-                ]
-              ) table'
-            )
-        ]
-        )
+              ) table')
+        ])
      )
   )
 
